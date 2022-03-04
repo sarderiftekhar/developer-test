44f9a8894de9500012974d5955470e529ea96b2b
Matt Ostrom-Hall <matt.ostrom-hall@reallysimplesystems.com>
2022-03-04 10:54:45 +0000

    updates

--- routes/web.php
+++ routes/web.php
@@ -26,40 +26,38 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware('auth')->group(function() {
-    Route::get('/dashboard', function() {
-        return Inertia::render('Dashboard');
-    })->middleware('auth')->name('dashboard');
+Route::get('/dashboard', function() {
+    return Inertia::render('Dashboard');
+})->middleware('auth')->name('dashboard');
 
-    Route::get('accounts', [AccountController::class, 'index'])
-        ->name('accounts.index');
-    Route::post('accounts', [AccountController::class, 'store'])
-        ->name('accounts.store');
-    Route::get('accounts/create', [AccountController::class, 'create'])
-        ->name('accounts.create');
-    Route::get('accounts/{account}/edit', [AccountController::class, 'edit'])
-        ->name('accounts.edit');
-    Route::get('accounts/{account}', [AccountController::class, 'show'])
-        ->name('accounts.show');
-    Route::put('accounts/{account}', [AccountController::class, 'update'])
-        ->name('accounts.update');
-    Route::delete('accounts/{account}', [AccountController::class, 'destroy'])
-        ->name('accounts.destroy');
+Route::get('accounts', [AccountController::class, 'index'])
+    ->name('accounts.index');
+Route::post('accounts', [AccountController::class, 'store'])
+    ->name('accounts.store');
+Route::get('accounts/create', [AccountController::class, 'create'])
+    ->name('accounts.create');
+Route::get('accounts/{account}/edit', [AccountController::class, 'edit'])
+    ->name('accounts.edit');
+Route::get('accounts/{account}', [AccountController::class, 'show'])
+    ->name('accounts.show');
+Route::put('accounts/{account}', [AccountController::class, 'update'])
+    ->name('accounts.update');
+Route::delete('accounts/{account}', [AccountController::class, 'destroy'])
+    ->name('accounts.destroy');
 
-    Route::get('contacts', [ContactController::class, 'index'])
-        ->name('contacts.index');
-    Route::post('contacts', [ContactController::class, 'store'])
-        ->name('contacts.store');
-    Route::get('contacts/create', [ContactController::class, 'create'])
-        ->name('contacts.create');
-    Route::get('contacts/{contact}/edit', [ContactController::class, 'edit'])
-        ->name('contacts.edit');
-    Route::get('contacts/{contact}', [ContactController::class, 'show'])
-        ->name('contacts.show');
-    Route::put('contacts/{contact}', [ContactController::class, 'update'])
-        ->name('contacts.update');
-    Route::delete('contacts/{contact}', [ContactController::class, 'destroy'])
-        ->name('contacts.destroy');
-});
+Route::get('contacts', [ContactController::class, 'index'])
+    ->name('contacts.index');
+Route::post('contacts', [ContactController::class, 'store'])
+    ->name('contacts.store');
+Route::get('contacts/create', [ContactController::class, 'create'])
+    ->name('contacts.create');
+Route::get('contacts/{contact}/edit', [ContactController::class, 'edit'])
+    ->name('contacts.edit');
+Route::get('contacts/{contact}', [ContactController::class, 'show'])
+    ->name('contacts.show');
+Route::put('contacts/{contact}', [ContactController::class, 'update'])
+    ->name('contacts.update');
+Route::delete('contacts/{contact}', [ContactController::class, 'destroy'])
+    ->name('contacts.destroy');
 
 require __DIR__.'/auth.php';
